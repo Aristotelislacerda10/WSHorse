de44bd0ccfac250eeeeeeb109bc76af68974c9cd
aristotelislacerda10@gmail.com <lacerda84187384>
2022-06-21 21:10:27 -0300

    [*] inclusão das rotinas de cadastrar, excluir e alterar clientes

--- Controllers/ClienteController.pas
+++ Controllers/ClienteController.pas
@@ -30,6 +30,7 @@ begin
     ArrayClientes := QueryCli.toJSONArray();
 
     res.send<TJSONArray>(ArrayClientes);
+    writeln(dateTimeToStr(now)+ ' - requisição: "Listar clientes" realizada na porta: ' + THorse.Port.ToString);
   finally
     QueryCli.free;
     Cliente.free;
@@ -60,6 +61,7 @@ begin
     begin
       ObjClientes := QueryCli.toJSONObject();
       res.send<TJSONObject>(ObjClientes);
+      writeln(dateTimeToStr(now)+ ' - requisição: "ListarClientesID" realizada na porta: ' + THorse.Port.ToString);
     end
     else
      res.send('Cliente nao encontrado').Status(404);
@@ -72,18 +74,139 @@ begin
 end;
 
 procedure CadastrarClientes(Req: THorseRequest; Res: THorseResponse; Next: TProc);
+var
+    cli : TCliente;
+    objCliente: TJSONObject;
+    erro : string;
+    body  : TJsonValue;
 begin
-  Res.Send('cadastrar clientes...');
+  // Conexao com o banco...
+  try
+      cli := TCliente.Create;
+  except
+      res.Send('Erro ao conectar com o banco').Status(500);
+      exit;
+  end;
+
+
+  try
+     try
+        body := TJSONObject.ParseJSONValue(TEncoding.UTF8.GetBytes(req.Body), 0) as TJsonValue;
+
+        cli.NOME := body.GetValue<string>('nome', '');
+        cli.EMAIL := body.GetValue<string>('email', '');
+        cli.FONE := body.GetValue<string>('fone', '');
+        cli.Inserir(erro);
+
+        body.Free;
+
+        if erro <> '' then
+            raise Exception.Create(erro);
+
+     except
+       on ex:exception do
+       begin
+          res.Send(ex.Message).Status(400);
+          exit;
+       end;
+     end;
+
+
+      objCliente := TJSONObject.Create;
+      objCliente.AddPair('id_cliente', cli.ID_CLIENTE.ToString);
+      writeln(dateTimeToStr(now)+ ' - requisição: "CadastrarClientes" realizada na porta: ' + THorse.Port.ToString);
+
+      res.Send<TJSONObject>(objCliente).Status(201);
+  finally
+      cli.Free;
+  end;
 end;
 
 procedure deletarClientes(Req: THorseRequest; Res: THorseResponse; Next: TProc);
+var
+    cli : TCliente;
+    objCliente: TJSONObject;
+    erro : string;
 begin
-  Res.Send('deletar clientes...');
+    // Conexao com o banco...
+    try
+        cli := TCliente.Create;
+    except
+        res.Send('Erro ao conectar com o banco').Status(500);
+        exit;
+    end;
+
+    try
+        try
+            cli.ID_CLIENTE := Req.Params['id'].ToInteger;
+
+            if NOT cli.Excluir(erro) then
+                raise Exception.Create(erro);
+
+        except on ex:exception do
+            begin
+                res.Send(ex.Message).Status(400);
+                exit;
+            end;
+        end;
+
+
+        objCliente := TJSONObject.Create;
+        objCliente.AddPair('id_cliente', cli.ID_CLIENTE.ToString);
+        writeln(dateTimeToStr(now)+ ' - requisição: "deletarClientes" realizada na porta: ' + THorse.Port.ToString);
+
+        res.Send<TJSONObject>(objCliente);
+    finally
+        cli.Free;
+    end;
 end;
 
 procedure AlterarClientes(Req: THorseRequest; Res: THorseResponse; Next: TProc);
+var
+    cli : TCliente;
+    objCliente: TJSONObject;
+    erro : string;
+    body : TJsonValue;
 begin
-  Res.Send('Altera clientes...');
+    // Conexao com o banco...
+    try
+        cli := TCliente.Create;
+    except
+        res.Send('Erro ao conectar com o banco').Status(500);
+        exit;
+    end;
+
+    try
+        try
+            body := TJSONObject.ParseJSONValue(TEncoding.UTF8.GetBytes(req.Body), 0) as TJsonValue;
+
+            cli.ID_CLIENTE := body.GetValue<integer>('id_cliente', 0);
+            cli.NOME := body.GetValue<string>('nome', '');
+            cli.EMAIL := body.GetValue<string>('email', '');
+            cli.FONE := body.GetValue<string>('fone', '');
+            cli.Editar(erro);
+
+            body.Free;
+
+            if erro <> '' then
+                raise Exception.Create(erro);
+
+        except on ex:exception do
+            begin
+                res.Send(ex.Message).Status(400);
+                exit;
+            end;
+        end;
+
+
+        objCliente := TJSONObject.Create;
+        objCliente.AddPair('id_cliente', cli.ID_CLIENTE.ToString);
+        writeln(dateTimeToStr(now)+ ' - requisição: "AlterarClientes" realizada na porta: ' + THorse.Port.ToString);
+
+        res.Send<TJSONObject>(objCliente).Status(200);
+    finally
+        cli.Free;
+    end;
 end;
 
 procedure Registry;
@@ -94,7 +217,7 @@ begin
 
   THorse.Post('/cliente', CadastrarClientes);
 
-  THorse.Delete('/cliente',deletarClientes);
+  THorse.Delete('/cliente/:id',deletarClientes);
 
   THorse.Put('/cliente',AlterarClientes);
 
